0e012a0e01cc75488315c6c8468d1bcc244f0e08
Evie-Skinner18 <eskinner@spartaglobal.com>
2020-03-08 15:13:37 +0000

    Changed it so that whenever my webhook receives a text, it sends a different inspirational quote in response

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
 require('dotenv').config();
-const accountSid = process.env.TWILIO_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-
-const twilio = require('twilio');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
-let twilioClient = new twilio(accountSid, authToken);
-let recipient = process.env.RECIPIENT;
-let sender = process.env.SENDER;
-
 const express = require('express');
 const app = express();
-
 const quoteLibrary = require('inspirational-quotes');
-let quote = quoteLibrary.getQuote();
-console.log(`Today's quote is from the great ${quote.author}: "${quote.text}"`);
 
 app.post('/sms', (req, res) => {
     let twimlResponse = new MessagingResponse();
+    let quote = quoteLibrary.getQuote();
+    console.log(`Today's quote is from the great ${quote.author}: "${quote.text}"`);
+
     twimlResponse.message(`Today's quote is from the great ${quote.author}: ${quote.text}`);
     
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twimlResponse.toString());
 });
-
-// twilioClient.messages.create({
-//     body: `Today's quote is from the great ${quote.author}: ${quote.text}`,
-//     to: recipient,
-//     from: sender,
-// })
-// .then((message) => console.log(message.sid));
 
 
 
